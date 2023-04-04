c34d52ce69a0fa2d5d375fbda4dc7bc976d85b78
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-04-04 12:24:04 +0900

    dd

--- build/users/seeProfile/seeProfile.typeDefs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_tag_1 = require("graphql-tag");
-exports.default = (0, graphql_tag_1.gql) `
-  type Query {
-    users: [User!]!
-    seeProfile(username: String!): User
-  }
-`;
